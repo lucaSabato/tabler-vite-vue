28aa85cc21b45a83bed28af3033739c15df62c19
Luca Sabato <luca.s@fdpgroup.co.uk>
2023-09-18 10:15:56 +0100

    added export and update the README, bump version

--- dist/core.js
+++ dist/core.js
@@ -4,36 +4,7 @@ import '../src/scss/core.scss'
 // Import only the Bootstrap components we need
 import { Alert, Button, Carousel, Collapse, Dropdown, Modal, Offcanvas, Popover, ScrollSpy, Tab, Toast, Tooltip } from 'bootstrap';
 
-// Alert
-document.querySelectorAll('.alert')
-    .forEach(alert => new Alert(alert))
-
-// Button
-document.querySelectorAll('[data-bs-toggle="button"]')
-    .forEach(button => {
-        new Button(button)
-    })
-
-// Carousel
-document.querySelectorAll('.carousel')
-    .forEach(carousel => {
-        new Carousel(carousel)
-    })
-
-// Collapse
-document.querySelectorAll('.collapse')
-    .forEach(collapse => {
-        new Collapse(collapse)
-    })
-
-// Popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-    .forEach(popover => {
-        new Popover(popover)
-    })
-
-// Tooltip
-document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    .forEach(tooltip => {
-        new Tooltip(tooltip)
-    })
+// Export components
+export {
+    Alert, Button, Carousel, Collapse, Dropdown, Modal, Offcanvas, Popover, ScrollSpy, Tab, Toast, Tooltip
+}
