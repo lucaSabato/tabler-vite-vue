fb007eca7a3f464d67644bc38cb3b39bee314992
Luca Sabato <luca.s@fdpgroup.co.uk>
2025-01-17 14:13:27 +0000

    Improved component initialization

--- dist/core.js
+++ dist/core.js
@@ -4,7 +4,55 @@ import '../src/scss/core.scss'
 // Import only the Bootstrap components we need
 import { Alert, Button, Carousel, Collapse, Dropdown, Modal, Offcanvas, Popover, ScrollSpy, Tab, Toast, Tooltip } from 'bootstrap';
 
+// Initialize Bootstrap components
+const initializeComponent = (selector, Component) => {
+    document.querySelectorAll(selector).forEach(element => {
+        try {
+            new Component(element)
+        } catch (error) {
+            console.error(`Failed to initialize ${Component.name}:`, error)
+        }
+    })
+}
+
+// Map of components and their selectors
+const components = {
+    '[data-bs-toggle="alert"]': Alert,
+    '[data-bs-toggle="button"]': Button,
+    '[data-bs-toggle="carousel"]': Carousel,
+    '[data-bs-toggle="collapse"]': Collapse,
+    '[data-bs-toggle="dropdown"]': Dropdown,
+    '[data-bs-toggle="modal"]': Modal,
+    '[data-bs-toggle="offcanvas"]': Offcanvas,
+    '[data-bs-toggle="popover"]': Popover,
+    '[data-bs-toggle="scrollspy"]': ScrollSpy,
+    '[data-bs-toggle="tab"]': Tab,
+    '[data-bs-toggle="toast"]': Toast,
+    '[data-bs-toggle="tooltip"]': Tooltip
+}
+
+// Initialize all components
+Object.entries(components).forEach(([selector, Component]) => {
+    initializeComponent(selector, Component)
+})
+
 // Export components
 export {
     Alert, Button, Carousel, Collapse, Dropdown, Modal, Offcanvas, Popover, ScrollSpy, Tab, Toast, Tooltip
+}
+
+// Add default export with all components
+export default {
+    Alert,
+    Button,
+    Carousel,
+    Collapse,
+    Dropdown,
+    Modal,
+    Offcanvas,
+    Popover,
+    ScrollSpy,
+    Tab,
+    Toast,
+    Tooltip
 }
--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,6 @@ import App from './App.vue'
 
 import { Alert, Button, Carousel, Collapse, Dropdown, Modal, Offcanvas, Popover, ScrollSpy, Tab, Toast, Tooltip } from '../../dist/core.js'
 
-
+// Initialize Vue app
 createApp(App).mount('#app')
 
-// Popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-    .forEach(popover => {
-        new Popover(popover)
-    })
-
-// Tooltip
-document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    .forEach(tooltip => {
-        new Tooltip(tooltip)
-    })
